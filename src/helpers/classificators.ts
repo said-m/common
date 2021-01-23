832b0b69c86ddbff73a4e83685891c5d185f4ccb
Said Magomedov <saids.inbox@gmail.com>
2021-01-24 00:46:50 +0300

    ✨ Проверка на принадлежность значения строковому `enum`-у

--- src/helpers/classificators.ts
+++ src/helpers/classificators.ts
@@ -9,7 +9,7 @@ export function isText(value: unknown): value is TextInterface {
 }
 
 /**
- * Проверяет, является ли значение элементом `Enum`.
+ * Проверяет, является ли строка ключом в `Enum`-е.
  *
  * например:
  * ```ts
@@ -30,3 +30,39 @@ export const isEnumItem = <
   item: Item,
   enumerable: Enumerable,
 ): item is Extract<keyof Enumerable, Item> => getKeys(enumerable).includes(item);
+
+
+/**
+ * Проверяет, является ли строка значением в `Enum`-е.
+ *
+ * например:
+ * ```ts
+ * enum A {
+ *   example = 'EXAMPLE',
+ *   test = 'TEST',
+ * }
+ * var value: string;        // >> `EXAMPLE`
+ *
+ * const type: A = value;    // type error;
+ *
+ * if (isEnumValue(value, A)) {
+ *   const type: A = value;  // ok; `value: A`
+ * }
+ * ```
+ */
+export const isEnumValue = <
+  Enumerable extends ObjectInterface,
+>(
+  value: unknown,
+  enumerable: Enumerable,
+): value is (
+  Extract<
+    Enumerable[keyof Enumerable],
+    string
+  > extends never
+  ? Enumerable
+  : Extract<
+    Enumerable[keyof Enumerable],
+    string
+  >
+) => Object.values(enumerable).includes(value);
